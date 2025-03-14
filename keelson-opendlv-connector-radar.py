90ec3e09cea597d02a335e33d3099c36c8264ea6
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2025-03-14 19:39:18 +0100

    Update keelson-opendlv-connector-radar.py

--- keelson-opendlv-connector-radar.py
+++ keelson-opendlv-connector-radar.py
@@ -46,7 +46,7 @@ PointCloudMsgID = 1061
 # Main loop that sets up zenoh arguments and then spins the primary loop. 
 
 if __name__ == "__main__":
-   
+
    parser = argparse.ArgumentParser(
       prog="opendlv-keelson-connector-radar",
       formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -89,114 +89,114 @@ if __name__ == "__main__":
 
       buffer = deque(maxlen=5000)
 
-   ################################################################################
-   # Functions to recieve envelopes
+      ################################################################################
+      # Functions to recieve envelopes
 
-   
+      
 
-   def put_to_zenoh(key: str, payload: bytes, session: zenoh.Session, args: argparse.Namespace):
-      envelope = keelson.enclose(payload)
-      logging.debug("...enclosed into envelope")
+      def put_to_zenoh(key: str, payload: bytes, session: zenoh.Session, args: argparse.Namespace):
+         envelope = keelson.enclose(payload)
+         logging.debug("...enclosed into envelope")
 
-      session.put(
-         key,
-         envelope,
-         priority=zenoh.Priority.REAL_TIME,
-         congestion_control=zenoh.CongestionControl.BLOCK,
-      )
-      logging.debug("...published to zenoh!")
-
-   def onMessage(msg, v1, v2):
-      logging.debug("Received..")
-      logging.debug (v1) ## ID
-      logging.debug (v2) ## timings
-      logging.debug (len(msg.data)) ## msg
-
-      point_cloud = PointCloud()
-      point_cloud.timestamp.FromDatetime(v2[2])
-      # Zero relative position
-      point_cloud.pose.position.x = 0
-      point_cloud.pose.position.y = 0
-      point_cloud.pose.position.z = 0
-
-
-      # Identity quaternion
-      point_cloud.pose.orientation.x = 0
-      point_cloud.pose.orientation.y = 0
-      point_cloud.pose.orientation.z = 0
-      point_cloud.pose.orientation.w = 1
-
-      # Fields
-      point_cloud.fields.add(name="x", offset=0, type=PackedElementField.NumericType.FLOAT32)
-      point_cloud.fields.add(name="y", offset=4, type=PackedElementField.NumericType.FLOAT32)
-      point_cloud.fields.add(name="i", offset=8, type=PackedElementField.NumericType.FLOAT32)
-
-      point_cloud.point_stride = 12
-      point_cloud.data = msg.data
-
-      buffer.append((point_cloud, v1))\
-
-
-   ################################################################################
-   # Start openDLV session
-   odsession = OD4Session.OD4Session(cid=zargs.cid)
-   print("Params set, beginning listener")
-   odsession.registerMessageCallback(PointCloudMsgID, onMessage, opendlv_message_standard_pb2.opendlv_proxy_PointCloudAngularLayeredReading)
-   odsession.connect()
-
-   key1 = keelson.construct_pubsub_key(
-            realm=zargs.realm,
-            entity_id=zargs.entity_id,
-            subject="point_cloud",
-            source_id=zargs.srca,
+         session.put(
+            key,
+            envelope,
+            priority=zenoh.Priority.REAL_TIME,
+            congestion_control=zenoh.CongestionControl.BLOCK,
          )
-   logging.debug(f"key1:{ key1}")
+         logging.debug("...published to zenoh!")
 
-   key2 = keelson.construct_pubsub_key(
-            realm=zargs.realm,
-            entity_id=zargs.entity_id,
-            subject="point_cloud",
-            source_id=zargs.srcb,
-         )
+      def onMessage(msg, v1, v2):
+         logging.debug("Received..")
+         logging.debug (v1) ## ID
+         logging.debug (v2) ## timings
+         logging.debug (len(msg.data)) ## msg
 
-   logging.debug(f"key2:{ key2}")
+         point_cloud = PointCloud()
+         point_cloud.timestamp.FromDatetime(v2[2])
+         # Zero relative position
+         point_cloud.pose.position.x = 0
+         point_cloud.pose.position.y = 0
+         point_cloud.pose.position.z = 0
 
-   pub1 = zsession.declare_publisher(key1,
-         priority=zenoh.Priority.REAL_TIME,
-         congestion_control=zenoh.CongestionControl.DROP)
-         
-   pub2 = zsession.declare_publisher(key2,
-         priority=zenoh.Priority.REAL_TIME,
-         congestion_control=zenoh.CongestionControl.DROP)
-         
 
-   while (odsession.isRunning and odsession.isConnected):
-      time.sleep(0.001)
-      while buffer:
-         try:
-            point_cloud, idx = buffer.popleft() ## Oldest first.
-            _currPC = point_cloud.timestamp.toDateTime()
-            _currSysC = datetime.now
-            asyncCurr= _currSysC - _currPC
-            logging.debug(f"Current Async: {asyncCurr}" )
-            payload = point_cloud.SerializeToString()
-            envelope = keelson.enclose(payload)
-            logging.debug(f"id:{idx}")
-         except:
-            print("Could not serialise the point_cloud")
-            continue
-         if idx == 1201:
-            pub1.put(envelope)
-         elif idx == 1202:
-            pub2.put(envelope)
-         logging.debug("Published!")
+         # Identity quaternion
+         point_cloud.pose.orientation.x = 0
+         point_cloud.pose.orientation.y = 0
+         point_cloud.pose.orientation.z = 0
+         point_cloud.pose.orientation.w = 1
 
-   logging.debug("Connection broken: Closing")
+         # Fields
+         point_cloud.fields.add(name="x", offset=0, type=PackedElementField.NumericType.FLOAT32)
+         point_cloud.fields.add(name="y", offset=4, type=PackedElementField.NumericType.FLOAT32)
+         point_cloud.fields.add(name="i", offset=8, type=PackedElementField.NumericType.FLOAT32)
 
-   def _on_exit():
-      zsession.close()
+         point_cloud.point_stride = 12
+         point_cloud.data = msg.data
 
-   atexit.register(_on_exit)
+         buffer.append((point_cloud, v1))\
+
+
+      ################################################################################
+      # Start openDLV session
+      odsession = OD4Session.OD4Session(cid=zargs.cid)
+      print("Params set, beginning listener")
+      odsession.registerMessageCallback(PointCloudMsgID, onMessage, opendlv_message_standard_pb2.opendlv_proxy_PointCloudAngularLayeredReading)
+      odsession.connect()
+
+      key1 = keelson.construct_pubsub_key(
+               realm=zargs.realm,
+               entity_id=zargs.entity_id,
+               subject="point_cloud",
+               source_id=zargs.srca,
+            )
+      logging.debug(f"key1:{ key1}")
+
+      key2 = keelson.construct_pubsub_key(
+               realm=zargs.realm,
+               entity_id=zargs.entity_id,
+               subject="point_cloud",
+               source_id=zargs.srcb,
+            )
+
+      logging.debug(f"key2:{ key2}")
+
+      pub1 = zsession.declare_publisher(key1,
+            priority=zenoh.Priority.REAL_TIME,
+            congestion_control=zenoh.CongestionControl.DROP)
+            
+      pub2 = zsession.declare_publisher(key2,
+            priority=zenoh.Priority.REAL_TIME,
+            congestion_control=zenoh.CongestionControl.DROP)
+            
+
+      while (odsession.isRunning and odsession.isConnected):
+         time.sleep(0.001)
+         while buffer:
+            try:
+               point_cloud, idx = buffer.popleft() ## Oldest first.
+               _currPC = point_cloud.timestamp.toDateTime()
+               _currSysC = datetime.now
+               asyncCurr= _currSysC - _currPC
+               logging.debug(f"Current Async: {asyncCurr}" )
+               payload = point_cloud.SerializeToString()
+               envelope = keelson.enclose(payload)
+               logging.debug(f"id:{idx}")
+            except:
+               print("Could not serialise the point_cloud")
+               continue
+            if idx == 1201:
+               pub1.put(envelope)
+            elif idx == 1202:
+               pub2.put(envelope)
+            logging.debug("Published!")
+
+      logging.debug("Connection broken: Closing")
+
+      def _on_exit():
+         zsession.close()
+
+      atexit.register(_on_exit)
 
 
 
